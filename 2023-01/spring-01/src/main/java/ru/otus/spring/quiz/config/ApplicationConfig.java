d4d0ee1c557187a63530232360bf356d09d2e7ca
Anton Danilov <danilovantonne@gmail.com>
2023-03-05 21:49:35 +0300

    add localization config

--- 2023-01/spring-01/src/main/java/ru/otus/spring/quiz/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.quiz.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:config/application.properties")
-class ApplicationConfig {
-}
---
+++ 2023-01/spring-01/src/main/java/ru/otus/spring/quiz/config/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package ru.otus.spring.quiz.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(ApplicationProperties.class)
+public class ApplicationConfiguration {
+}
